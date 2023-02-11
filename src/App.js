83986a303fa91c40d7c3e62d111cc9ceda10f22e
James Shi <jamesshi117@gmail.com>
2023-02-11 00:01:46 -0800

    Section work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import React from 'react';
-import { AppBar, Typography, Toolbar, Box, Grid, Paper } from '@mui/material';
+import { AppBar, Typography, Toolbar, Box, Grid, Paper, Button, Card, CardActionArea, CardMedia, CardContent } from '@mui/material';
 import styled from 'styled-components';
 import AdbIcon from '@mui/icons-material/Adb';
+import { style } from '@mui/system';
 
 function App() {
 
@@ -18,86 +19,190 @@ function App() {
   @media (min-width: 960px) {
     padding: 6rem;
   }
-  background-color: red;
+  background-color: white;
   & > * {
     padding-bottom: ${props => props.spacing}px;
   }
 `;
 
   const GridItem = styled(Grid)`
+  display: flex;
   flex-basis: 50%;
   background-color: white;
+  align-items: center;
+  justify-content: center;
 `
+  const OptimaFont = styled('p')({
+    fontFamily: 'OptimaLight',
+    lineHeight: 1,
+    margin: 0
+  });
+
+  const HelveticaFont = styled('p')({
+    fontFamily: 'Helvetica',
+    margin: 0,
+  });
+
+  const ColorButton = styled(Button)(({ theme }) => ({
+    width: "30%",
+    backgroundColor: "#8B5E3C !important",
+    '&:hover': {
+      backgroundColor: "red !important",
+    },
+  }));
+
+  const StyledCard = styled(Card)`
+  max-width: 345px;
+  transition: 0.3s;
+  &:hover {
+    transform: translateY(-10px);
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+  }
+`;
 
   return (
     <Root>
       <GridContainer container direction="column" spacing={0}>
-        <Grid container direction="row" display="flex" height="60vh" width="100%" sx={{ mb: 4 }}>
+        <Grid container direction="row" display="flex" height="100vh" width="100%" sx={{ mb: 4 }}>
           <AnotherAppBar />
-          <GridItem item xs={12} sm={6} md={6} lg={6} xl={6}>
-            Title text goes here
+          <GridItem item xs={12} sm={12} md={6} lg={6} xl={6}>
+            <Grid container direction="column" display="flex" height="100%" justifyContent="center" sx={{ backgroundColor: "" }}>
+              <Typography variant="h3" style={{ fontSize: 60 }}>
+                <OptimaFont>
+                  Healing Hands, Tranquil Minds
+                </OptimaFont>
+              </Typography>
+              <div style={{ height: 20 }} />
+              <Typography variant="h6" style={{ fontSize: 30 }}>
+                <OptimaFont>
+                  Oasis Massage Therapy
+                </OptimaFont>
+              </Typography>
+              <div style={{ height: 10 }} />
+              <ColorButton variant="contained" size="large">
+                <HelveticaFont>Book Now</HelveticaFont>
+              </ColorButton>
+            </Grid>
           </GridItem>
-          <GridItem item xs={12} sm={6} md={6} lg={6} xl={6} sx={{ backgroundColor: "purple" }}>
-            <img src={process.env.PUBLIC_URL + '/massage.jpg'} alt="Your image description" width="100%" />
+          <GridItem item xs={12} sm={12} md={6} lg={6} xl={6} sx={{ backgroundColor: "blue" }}>
+            <img src={process.env.PUBLIC_URL + '/massage.jpg'} alt="Your image description" width="90%" />
           </GridItem>
-
-
         </Grid>
-        <Grid container direction="row" display="flex" height="30vh" width="100%" sx={{ mb: 4 }}>
-          <GridItem item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ backgroundColor: "gold" }}>
-            About Section
-          </GridItem>
+
+        <Grid container direction="column" display="flex" width="100%" maxWidth={1000} alignSelf="center" sx={{ mb: 4, backgroundColor: "" }}>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <Typography variant="h6" style={{ fontSize: 30, textAlign: "center" }}>
+              <OptimaFont>
+                Meet Your Therapist
+              </OptimaFont>
+            </Typography>
+            <div style={{ height: 20 }} />
+            <Typography variant="h8" style={{ fontSize: 18, textAlign: "center", lineHeight: 1.8 }}><HelveticaFont >I am a graduate of the West Coast College of Massage Therapy (WCCMT) in Victoria BC and a Registered
+              Massage Therapist.  I work and communicate with clients to provide exceptional care by implementing
+              individualized treatment  plans to accomplish their healthcare goals. I specialize in many techniques
+              including trigger point  modalities, Swedish massage, Myofascial release therapy, neuromuscular
+              therapy, active and passive  stretching and deep tissue work. My goal is to help my clients manage
+              stress, chronic pain and mental  health challenges as well as improve their general wellbeing</HelveticaFont></Typography>
+          </Grid>
         </Grid>
-        <Grid container direction="row" display="flex" height="120vh" justifyContent="center" sx={{ mb: 4, backgroundColor: "cyan" }}>
-          <GridItem item xs={4} sm={4} md={12} lg={3} xl={4} sx={{ backgroundColor: "blue", margin: 3 }}>
-            <Paper
-              sx={{
-                height: "40vh",
-                width: "100%",
-                backgroundColor: (theme) =>
-                  theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-              }}
-            />
-          </GridItem>
-          <GridItem item xs={4} sm={4} md={12} lg={3} xl={4} sx={{ backgroundColor: "blue", margin: 3 }}>
-            <Paper
-              sx={{
-                height: "40vh",
-                width: "100%",
-                backgroundColor: (theme) =>
-                  theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-              }}
-            />
-          </GridItem>
-          <GridItem item xs={4} sm={4} md={12} lg={3} xl={4} sx={{ backgroundColor: "blue", margin: 3 }}>
-            <Paper
-              sx={{
-                height: "40vh",
-                width: "100%",
-                backgroundColor: (theme) =>
-                  theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-              }}
-            />          </GridItem>
-          <GridItem item xs={4} sm={4} md={12} lg={3} xl={4} sx={{ backgroundColor: "blue", margin: 3 }}>
-            <Paper
-              sx={{
-                height: "40vh",
-                width: "100%",
-                backgroundColor: (theme) =>
-                  theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-              }}
-            />
-          </GridItem>
-          <GridItem item xs={4} sm={4} md={12} lg={3} xl={4} sx={{ backgroundColor: "blue", margin: 3 }}>
-            <Paper
-              sx={{
-                height: "40vh",
-                width: "100%",
-                backgroundColor: (theme) =>
-                  theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-              }}
-            />
-          </GridItem>
+        <Grid container direction="column" display="flex" width="100%" alignSelf="center" sx={{ mb: 4, backgroundColor: "" }}>
+          <Typography variant="h6" style={{ fontSize: 30, textAlign: "center" }}>
+            <OptimaFont>
+              Available Treatments
+            </OptimaFont>
+          </Typography>
+          <Grid container direction="row" display="flex" justifyContent="center" sx={{ backgroundColor: "" }}>
+            <GridItem item xs={12} sm={12} md={12} lg={3} xl={4} sx={{ backgroundColor: "", margin: 3 }}>
+              <StyledCard sx={{ maxWidth: 345, height: 350 }} style={{ transition: "all 0.5s ease-in-out" }}>
+                <CardActionArea>
+                  <CardContent>
+                    <Box height={140} sx={{ backgroundColor: "brown" }} />
+                  </CardContent>
+                  <CardContent sx={{ backgroundColor: "grey", height: "100%" }}>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Swedish Massage
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      A therapeutic massage that is designed to relax the entire body...                      A therapeutic massage that is designed to relax the entire body...
+
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </StyledCard>
+            </GridItem>
+            <GridItem item xs={12} sm={12} md={12} lg={3} xl={4} sx={{ backgroundColor: "", margin: 3 }}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardContent>
+                    <Box height={140} sx={{ backgroundColor: "brown" }} />
+                  </CardContent>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Trigger Point Modalities
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000
+                      species, ranging across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </GridItem>
+            <GridItem item xs={12} sm={12} md={12} lg={3} xl={4} sx={{ backgroundColor: "", margin: 3 }}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardContent>
+                    <Box height={140} sx={{ backgroundColor: "brown" }} />
+                  </CardContent>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Trigger Point Modalities
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000
+                      species, ranging across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </GridItem>
+            <GridItem item xs={12} sm={12} md={12} lg={3} xl={4} sx={{ backgroundColor: "", margin: 3 }}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardContent>
+                    <Box height={140} sx={{ backgroundColor: "brown" }} />
+                  </CardContent>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Trigger Point Modalities
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000
+                      species, ranging across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </GridItem>
+            <GridItem item xs={12} sm={12} md={12} lg={3} xl={4} sx={{ backgroundColor: "", margin: 3 }}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardContent>
+                    <Box height={140} sx={{ backgroundColor: "brown" }} />
+                  </CardContent>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Trigger Point Modalities
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000
+                      species, ranging across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </GridItem>
+          </Grid>
         </Grid>
         <Grid container direction="row" display="flex" height="40vh" width="100%" sx={{ mb: 4 }}>
           <GridItem item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ backgroundColor: "pink" }}>
