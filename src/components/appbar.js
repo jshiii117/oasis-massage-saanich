8f4c955a3c4a8d5502889ce28c29dc4a9e43cd16
James Shi <jamesshi117@gmail.com>
2023-02-17 04:10:27 -0800

    Mobile menu

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { AppBar, Typography, Toolbar, Box, Grid, Button } from '@mui/material';
+import { AppBar, Toolbar, Box, Grid, Button, useTheme, useMediaQuery, IconButton, ListItem, ListItemButton, ListItemText, List, ListItemIcon } from '@mui/material';
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
 import { OptimaType } from '../styles/globalStyles';
+import MenuIcon from '@mui/icons-material/Menu';
+import InfoIcon from '@mui/icons-material/Info';
+import EventIcon from '@mui/icons-material/Event';
+import EmailIcon from '@mui/icons-material/Email';
 
 const StyledAppBar = styled(AppBar)(({ isVisible }) => ({
     "&&": {
         height: "12%",
+        minHeight: 62,
         maxHeight: 65,
         backgroundColor: 'white',
         opacity: isVisible ? 1 : 0,
         transition: "opacity 0.5s ease, background-color 0.5s ease",
-        // "&:hover": {
-        //     backgroundColor: "yellow",
-        // },
         position: "fixed",
         top: 0,
         left: 0,
@@ -28,19 +30,15 @@ const StyledToolbar = styled(Toolbar)({
     alignItems: "center"
 });
 
-const AppBarItem = styled(Typography)({
-    color: 'black',
-    fontSize: 20,
-    textTransform: 'none',
-    transition: 'color 0.3s linear',
-    '&:hover': {
-        color: '#A67C52'
-    }
-});
-
-const AppBarButton = styled(Button)({
-    backgroundColor: "transparent"
-})
+// const AppBarItem = styled(Typography)({
+//     color: 'black',
+//     fontSize: 20,
+//     textTransform: 'none',
+//     transition: 'color 0.3s linear',
+//     '&:hover': {
+//         color: '#A67C52'
+//     }
+// });
 
 function CustomAppBar() {
     const [isVisible, setIsVisible] = useState(true);
@@ -62,76 +60,114 @@ function CustomAppBar() {
         };
     }, []);
 
-    const [hover, setHover] = useState([false, false, false])
+    const theme = useTheme();
+
+    var isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
     return (
         <StyledAppBar isVisible={isVisible} position='fixed'>
-            <StyledToolbar>
+            <StyledToolbar sx={{ justifyContent: isSmallScreen ? 'space-between' : 'center' }}>
                 <Box display="flex">
                     <img src={process.env.PUBLIC_URL + '/logo.png'} alt="Placeholder alt text" style={{ width: 60, height: "auto" }}
                     />
                 </Box>
                 <Box display="flex" backgroundColor="">
-                    <div style={{ width: 20 }} />
-                    <Grid container direction="row" justify="space-between">
-                        <AppBarButton style={{
-                            backgroundColor: 'transparent'
-                        }
-                        }>
-                            <OptimaType variant="h8" onMouseEnter={() => setHover([true, false, false])}
-                                onMouseLeave={() => setHover([false, false, false])}
-                                style={{
-                                    color: hover[0] ? '#A67C52' : 'black',
-                                    fontSize: 20,
-                                    textTransform: 'none',
-                                    transition: 'color 0.3s linear',
-                                }}>
-                                About
-                            </OptimaType>
-                        </AppBarButton>
-                        <div style={{ width: 20 }} />
-                        <AppBarButton style={{
-                            backgroundColor: 'transparent'
-                        }
-                        }>
-                            <AppBarItem variant="h8"
-                                onMouseEnter={() => setHover([false, true, false])}
-                                onMouseLeave={() => setHover([false, false, false])}
-                            >
-                                <OptimaType variant="h8" style={{
-                                    color: hover[1] ? '#A67C52' : 'black',
-                                    fontSize: 20,
-                                    textTransform: 'none',
-                                    transition: 'color 0.3s linear',
-                                }}>
-                                    Services
-                                </OptimaType>
-                            </AppBarItem>
-                        </AppBarButton>
-                        <div style={{ width: 20 }} />
-                        <AppBarButton style={{
-                            backgroundColor: 'transparent'
-                        }
-                        }>
-                            <AppBarItem variant="h8"
-                                onMouseEnter={() => setHover([false, false, true])}
-                                onMouseLeave={() => setHover([false, false, false])}
-                            >
-                                <OptimaType variant="h8" style={{
-                                    color: hover[2] ? '#A67C52' : 'black',
-                                    fontSize: 20,
-                                    textTransform: 'none',
-                                    transition: 'color 0.3s linear',
-                                }}>
-                                    Contact
-                                </OptimaType>
-                            </AppBarItem>
-                        </AppBarButton>
-                    </Grid>
+                    {isSmallScreen ? (
+                        <MobileMenu />
+                    ) : (
+                        <Grid container direction="row" justify="center">
+                            <DesktopAppBarItem itemName={"About"} />
+                            <DesktopAppBarItem itemName={"Booking"} />
+                            <DesktopAppBarItem itemName={"Contact"} />
+                        </Grid>
+                    )}
                 </Box>
             </StyledToolbar>
         </StyledAppBar >
     )
+}
+
+function DesktopAppBarItem(props) {
+    const [hover, setHover] = useState(false);
+
+    return (
+        <Button style={{ backgroundColor: 'transparent' }
+        }>
+            <OptimaType variant="h8" onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+                style={{
+                    color: hover ? '#A67C52' : 'black',
+                    fontSize: 20,
+                    textTransform: 'none',
+                    transition: 'color 0.3s linear',
+                }}>
+                {props.itemName}
+            </OptimaType>
+        </Button>
+    )
+}
+
+function MobileMenu() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    return (
+        <>
+            <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                onClick={() => setMenuOpen(!menuOpen)}
+            >
+                <MenuIcon style={{ color: 'black', fontSize: 30 }} />
+            </IconButton>
+            {menuOpen && (
+                <Box
+                    sx={{
+                        position: 'fixed',
+                        top: 0,
+                        left: 0,
+                        width: '100vw',
+                        height: '100vh',
+                        backgroundColor: 'white',
+                        zIndex: 1,
+                    }}
+                >
+                    <Box display="flex" justifyContent="flex-end" p={2}>
+                        <IconButton onClick={() => setMenuOpen(!menuOpen)}>
+                            <MenuIcon />
+                        </IconButton>
+                    </Box>
+                    <List>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <InfoIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="About" />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <EventIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Booking" />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <EmailIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Contact" />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+                </Box>
+            )}
+        </>
+
+    )
+
 }
 
 export default CustomAppBar;
--- src/components/landingSection.js
+++ src/components/landingSection.js
@@ -1,6 +1,6 @@
 import '../App.css';
 import React from 'react';
-import { Typography, Grid, Button, useTheme, useMediaQuery } from '@mui/material';
+import { Grid, Button, useTheme, useMediaQuery } from '@mui/material';
 import styled from 'styled-components';
 import { HelveticaType, OptimaType } from '../styles/globalStyles';
 import theme from '../styles/theme';
@@ -25,31 +25,31 @@ const ColorButton = styled(Button)(({ theme }) => ({
 
 function LandingSection() {
     const theme = useTheme();
-    
+
     return (
         <Grid container direction="row" display="flex" alignSelf="center" alignItems="center" width="100%" maxWidth={1000} sx={{ mb: 4, backgroundColor: '' }}>
             <GridItem item xs={12} sm={12} md={6} lg={6} xl={6} sx={{ marginY: 10 }}>
                 <Grid container direction="column" display="flex" height="100%" alignItems={useMediaQuery(theme.breakpoints.up('md')) ? '' : 'center'}>
-                    <OptimaType className="optima" variant="h3" style={{ fontSize: 60 }}>
-                        Healing Hands, <br/>Tranquil Minds.
-                    </OptimaType>   
+                    <OptimaType className="optima" variant="h3" style={{ fontSize: 60, textAlign: useMediaQuery(theme.breakpoints.up('md')) ? '' : 'center' }}>
+                        Healing Hands, <br />Tranquil Minds.
+                    </OptimaType>
                     <OptimaType className="optima" variant="h6" style={{ fontSize: 30 }}>
                         Oasis Massage Therapy
                     </OptimaType>
                     <div style={{ height: 40 }} />
                     <Grid container direction="row" display="flex" justifyContent={useMediaQuery(theme.breakpoints.up('md')) ? '' : 'center'}>
-                    <ColorButton variant="contained" size="large">
-                        <HelveticaType >Book Now</HelveticaType>
-                    </ColorButton>
-                    <div style={{ width: 20 }} />
-                    <ColorButton variant="contained" size="large">
-                        <HelveticaType >Contact Us</HelveticaType>
-                    </ColorButton>
+                        <ColorButton variant="contained" size="large">
+                            <HelveticaType >Book Now</HelveticaType>
+                        </ColorButton>
+                        <div style={{ width: 20 }} />
+                        <ColorButton variant="contained" size="large">
+                            <HelveticaType >Contact Us</HelveticaType>
+                        </ColorButton>
                     </Grid>
                 </Grid>
             </GridItem>
             <GridItem item xs={12} sm={12} md={6} lg={6} xl={6} sx={{ backgroundColor: "" }}>
-                <img src={process.env.PUBLIC_URL + '/mainDesign.png'} alt="Placeholder alt text" width="80%"  />
+                <img src={process.env.PUBLIC_URL + '/mainDesign.png'} alt="Placeholder alt text" style={{ width: '100%', maxWidth: '100%', minWidth: 400 }} />
             </GridItem>
         </Grid>
     )
