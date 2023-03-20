22a4755ecb922cb5fbf3890c36f5d4d4c278aff5
James Shi <jamesshi117@gmail.com>
2023-03-20 11:06:24 -0700

    Adjust footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,23 @@
-import styled from 'styled-components';
+import { Typography } from "@mui/material";
+import styled from "styled-components";
 
 const FooterContainer = styled.footer`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 50px;
   background-color: #333;
   color: #fff;
+  flex-wrap: wrap;
+  padding: 1rem;
 `;
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <p>Copyright &copy; {new Date().getFullYear()} SpaName |</p>
+      <Typography variant="body" sx={{ textAlign: "center" }}>
+        Copyright &copy; {new Date().getFullYear()} Oasis Massage Therapy |
+        Developed by James Shi
+      </Typography>
     </FooterContainer>
   );
 };
